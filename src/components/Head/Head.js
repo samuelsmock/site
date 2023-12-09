0ef4d7060c8d40b280ded6bc9f5f15d3a73b983a
samuelsmock <smock.samuel@gmail.com>
2023-12-09 11:53:38 +0100

    minor change after thesis submission

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -18,7 +18,7 @@ class Head extends React.Component {
               
                 <div className ="info">
                     <h1 className = "large-print">Samuel &nbsp; Smock</h1>
-                    <p className = "description"> Geospatial Analysis & Modeling | Energy System Design & Impact | Sustainability  </p>
+                    <p className = "description"> Geospatial Analysis & Modeling | Energy System Design & Impact | Sustainability Assessment </p>
                     
                     <p className='contact'>
                         Contact: smock.samuel@gmail.com
